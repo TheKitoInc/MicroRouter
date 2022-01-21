d89e84644a28ccd3c45afa7dc96f1b603b936b10
TheKito <11139894+TheKito@users.noreply.github.com>
2022-01-21 12:14:33 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,53 +1,65 @@
 <?php
 
-ob_start();
+namespace Kito\MicroServices;
 
-function removeArgs(string $path): string
+class MicroRouter
 {
-    $elements = explode('?', $path, 2);
-    return $elements[0];
-}
 
-function parsePath(string $path): string
-{
-    $elements = array();
-    foreach (explode(DIRECTORY_SEPARATOR, str_replace("\\", DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, $path))) as $element)
+    public function __construct()
     {
-        if (empty($element))
-            continue;
+        ob_start();
 
-        if ($element == '.')
-            continue;
-
-        if ($element == '..')
-            array_pop($elements);
-
-        $elements[] = $element;
-    }
-
-    return implode(DIRECTORY_SEPARATOR, $elements);
-}
-
-$SITE_ROOT = realpath(parsePath(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT')));
+        $SITE_ROOT = realpath(self::parsePath(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT')));
 //error_log($SITE_ROOT);
 
-$REQUEST_PATH = parsePath(removeArgs(filter_input(INPUT_SERVER, 'REQUEST_URI')));
+        $REQUEST_PATH = self::parsePath(self::removeArgs(filter_input(INPUT_SERVER, 'REQUEST_URI')));
 //error_log($REQUEST_PATH);
 
-$PATH = parsePath($SITE_ROOT . DIRECTORY_SEPARATOR . $REQUEST_PATH);
+        $PATH = self::parsePath($SITE_ROOT . DIRECTORY_SEPARATOR . $REQUEST_PATH);
 //error_log($PATH);
 
 
-while (strlen($PATH) > strlen($_SERVER['DOCUMENT_ROOT']) + 1)
-{
-    $routerPath = $PATH . '/index.php';
+        while (strlen($PATH) > strlen($_SERVER['DOCUMENT_ROOT']) + 1)
+        {
+            $routerPath = $PATH . '/index.php';
 
-    if (file_exists($routerPath))
-    {
-        ob_clean();
-        require_once($routerPath);
-        return;
+            if (file_exists($routerPath))
+            {
+                ob_clean();
+                require_once($routerPath);
+                return;
+            }
+
+            $PATH = dirname($PATH);
+        }
     }
 
-    $PATH = dirname($PATH);
+    private static function removeArgs(string $path): string
+    {
+        $elements = explode('?', $path, 2);
+        return $elements[0];
+    }
+
+    private static function parsePath(string $path): string
+    {
+        $elements = array();
+        foreach (explode(DIRECTORY_SEPARATOR, str_replace("\\", DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, $path))) as $element)
+        {
+            if (empty($element))
+                continue;
+
+            if ($element == '.')
+                continue;
+
+            if ($element == '..')
+                array_pop($elements);
+
+            $elements[] = $element;
+        }
+
+        return implode(DIRECTORY_SEPARATOR, $elements);
+    }
+
 }
+
+new MicroRouter();
